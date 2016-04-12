c9564bf571e2e62a576fdd2ef371220588c38603
AlaynF <alayn@admitsolutions.com>
2016-04-11 21:18:56 -0400

    roast

--- assets/js/home.js
+++ assets/js/home.js
@@ -2,4 +2,8 @@
 
 window.app.controller('home', function($scope, $http, $location, $window) {
     document.title="Home";
+
+    var rats = ["rat, pig, mule"]
+    $scope.rat = rats[Math.floor(Math.random() * rats.length)];
+    
 })
